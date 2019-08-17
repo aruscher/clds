28a9fafbcc10597479c155173c6d11c0b89e9cce
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2019-08-17 21:11:12 +0200

    update package definition

--- src/package.lisp
+++ src/package.lisp
@@ -1,4 +1,21 @@
 ;;;; package.lisp
 
 (defpackage #:clds
-  (:use #:cl))
+  (:use #:cl)
+  (:export
+   ;Doubly-linked-list
+   #:make-dlist
+   #:dlist-size
+   #:dlist-empty-p
+   #:dlist-add-element-at
+   #:dlist-add-element-front
+   #:dlist-add-element-end
+   #:dlist-remove-element-at
+   #:dlist-remove-element-front
+   #:dlist-remove-element-end
+   #:dlist-get-elements
+   #:dlist-get-element-at
+   #:dlist-get-element-front
+   #:dlist-get-element-end
+   #:dlist-find-element
+   ))
